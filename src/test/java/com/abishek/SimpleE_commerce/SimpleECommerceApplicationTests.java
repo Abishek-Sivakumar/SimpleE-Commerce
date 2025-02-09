8284b8e2c95b5bd648f0cceed712ce69667aa159
ABISHEK <abideepu19.vrs@gmail.com>
2025-02-09 17:51:34 +0530

    Initial commit

---
+++ src/test/java/com/abishek/SimpleE_commerce/SimpleECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abishek.SimpleE_commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
